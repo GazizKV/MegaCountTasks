298f567074b3b8f99ee77a276f9ff231cddff593
gazizkv <valitovgaziz@gami.com>
2021-02-16 02:05:55 +0000

    need to add counter for side 	modified:   secondTask.c

--- secondTask.c
+++ secondTask.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 int main(int argc) {
 
 	int number;
-	int numbers[number][number];
+	int numbersArray[number][number];
 	int converter;
 	int value;
 	int counter;
@@ -13,26 +14,40 @@ int main(int argc) {
 
 	counter=1;
 	value = 1;
-	printf("Enter a Integer number:\t");
+	printf("Enter any Integer number:\t");
 	scanf("%d", &number);
+	printf("%d\n", number);
 	printSide = number;
 
-	while(counter<=number*number) {
+	while(1) {
 
 		for(int i=0;i<printSide;i++) {
 			numbersArray[counter][i] = value++;
 		}
-		for(int i=0;
+		if(value=number*number)
+			break;
+		--printSide;
+		for(int i=0;i<printSide;i++) {
+			numbersArray[i+counter][printSide] = value++;
+		}
+		if(value=number*number)
+			break;
+		for(int i=printSide;i>-1;i--) {
+			numbersArray[i][printSide] = value++;
+		}
+		--printSide;
+		printf("marker\n");
+		if(value=number*number)
+			break;
 			
 	}
 
-	for (int i=1; i<=number; i++) {
-		for(int j=1; j<=number; j++) {
-			printf("%d", numbers[j][i]);
+	for (int i=0; i<number; i++) {
+		for(int j=0; j<number; j++) {
+			printf("%d ", numbersArray[i][j]);
 		}
 		printf("\n");
 	}
-					
 
 	return 0;
 }
